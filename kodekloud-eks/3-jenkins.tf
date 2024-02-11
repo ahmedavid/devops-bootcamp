8c0a4829120fe2cbab13f3aed7cf62b07560ecba
David Ahmadov <ahmedavid@gmail.com>
2024-02-11 14:33:10 +0400

    jenkins fix

--- kodekloud-eks/3-jenkins.tf
+++ kodekloud-eks/3-jenkins.tf
@@ -1,3 +1,11 @@
-resource "kubectl_manifest" "jenkins" {
+resource "kubectl_manifest" "jenkins_deployment" {
   yaml_body  = file("values/jenkins/deployment.yaml")
+}
+
+resource "kubectl_manifest" "jenkins_service" {
+  yaml_body  = file("values/jenkins/service.yaml")
+}
+
+resource "kubectl_manifest" "jenkins_ingress" {
+  yaml_body  = file("values/jenkins/ingress.yaml")
 }
